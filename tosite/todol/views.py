673b1220789ff87311205332468bd5c0c005d462
Rajath <rajath@elementure.tech>
2022-08-10 18:37:10 +0530

    Boto3 Added

--- tosite/todol/views.py
+++ tosite/todol/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render  # noqa
 from django.http import HttpResponse
+import boto3
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello Django!")
+
+
+def Upload(request):
+    s3 = boto3.resource("s3")
+    file = request.FILES["file"]
+    s3.Bucket("todol").put_object(Key=file.name, Body=file)
+    return HttpResponse("Uploaded")
+
+
+def Read(request):
+    s3 = boto3.resource("s3")
+    obj = s3.Object("todol", "test.txt")
+    return HttpResponse(obj.get()["Body"].read().decode("utf-8"))
